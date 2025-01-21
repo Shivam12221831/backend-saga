02836611ceff1eb8ef3d198721391d91934c25c1
Shivam12221831 <shiva954882@gmail.com>
2025-01-21 15:09:08 +0530

    ejs views,public and include folder addded

--- Backend-3(EJS)/index.js
+++ Backend-3(EJS)/index.js
@@ -2,11 +2,30 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "/public/css")));
+app.use(express.static(path.join(__dirname, "/public/js")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
+})
+
+app.get("/rollDice", (req, res) => {
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", {diceVal});
+});
+
+app.get("/ig/:username", (req, res) => {
+    let {username} = req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    if(data){
+        res.render("instagram.ejs", { data });
+    }else{
+        res.render("error.ejs", {username});
+    }
 })
 
 const port = 3000;
