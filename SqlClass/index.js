4b6798d576f530bc11630ae810e5b931cbc012cf
Shivam12221831 <shiva954882@gmail.com>
2025-01-24 23:41:11 +0530

    SQL basics with use of REST APIs

--- SqlClass/index.js
+++ SqlClass/index.js
@@ -1,5 +1,17 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+const path = require("path");
+const methodOverride = require('method-override');
+
+app.use(methodOverride("_method"));
+
+app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 // let createRandomUser = () => {
 //     return {
@@ -24,24 +36,118 @@ const connection = mysql.createConnection({
     password : "root",
 });
 
-try{
-    connection.query("SHOW TABLES", (err, result) => {
-        if(err) throw err;
-        console.log(result);
-    })
-} catch(err){
-    console.log(err);
-}
-
-connection.end();
-
 let getRandomUser = () => {
-    return {
-      id: faker.string.uuid(),
-      username: faker.internet.username(), // before version 9.1.0, use userName()
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
+    return [
+        faker.string.uuid(),
+        faker.internet.username(), // before version 9.1.0, use userName()
+        faker.internet.email(),
+        faker.internet.password(),
+    ];
 }
 
 // console.log(getRandomUser());
+
+// GET - / - Fetch and show total number of users on our app
+app.get("/", (req, res) => {
+    let q = "SELECT Count(*) from user";
+    try{
+        connection.query(q, (err, result) => {
+            if(err) throw err;
+            // console.log(result[0]["Count(*)"]);
+            let count = result[0]["Count(*)"];
+            res.render("home.ejs", {count});
+        });
+    }catch(err){
+        console.log(err);
+        res.send("Some error in DB");
+    }
+})
+
+// Show route
+app.get("/user", (req,res) => {
+    let q = "SELECT * FROM user";
+    try{
+        connection.query(q, (err, users) => {
+            if(err) throw err;
+            // console.log(result);
+            res.render("showuser.ejs", { users });
+        });
+    } catch(err){
+        console.log(err);
+        res.send("some error is occur");
+    }
+});
+
+// Edit username
+// GET - /user/:id/edit  - to get form to edit username, based on id
+// - This form will require a password
+// PATCH - /user/:id  - to edit username, if correct password was entered in form
+
+app.get("/user/:id/edit", (req,res) => {
+    let { id } = req.params;
+    console.log(id);
+    let q = "SELECT * FROM user WHERE id=?";
+    try{
+        connection.query(q, id, (err, result) => {
+            if(err) throw err;
+            // console.log(result);
+            let user = result[0];
+            res.render("edit.ejs", { user });
+        });
+    } catch(err){
+        console.log(err);
+        res.send("some error occured!");
+    }
+});
+
+// app.patch("/user/:id", (req,res) => {
+//     let { id } = req.params;
+//     console.log(id);
+//     let user = req.body;
+//     let q = `UPDATE user SET name='${user.username}' WHERE password == ${user.password}`;
+//     try{
+//         connection.query(q, (err, result) => {
+//             if(err) throw err;
+//             console.log(result);
+//         });
+//     } catch(err){
+//         console.log(error);
+//         res.send("You have entered worng password");
+//     }
+//     // res.send("patch is working fine");
+// })
+
+const port = 8080;
+app.listen(port, () => {
+    console.log(`listening on port : ${port}`);
+});
+
+// let q = "SHOW TABLES";
+
+// // Inserting New Data
+// let q = "INSERT INTO user (id, name, email, password) VALUES (?, ?, ?, ?)";    // these ? are called placeholders
+// let user = ["123a", "123_newusera", "abd@gmail.com", "abca"];
+
+// If you want to add multi rows in your table
+// let q = "INSERT INTO user (id, name, email, password) VALUES ?";    // these ? are called placeholders
+// let users = [["123b", "123_newuserb", "abd@gmail.comb", "abcb"],
+// ["123c", "123_newuserc", "abd@gmail.comc", "abcc"]];
+
+//  Entering data in bulk using faker package
+// first truncate user table to add new data
+// let q = "INSERT INTO user (id, name, email, password) VALUES ?";    // these ? are called placeholders
+// let data = [];
+// for(let i=1; i<=100; i++){
+//     data.push(getRandomUser());
+// }
+
+// try{
+//     connection.query(q, [data], (err, result) => {
+//         if(err) throw err;
+//         console.log(result);
+//     })
+// } catch(err){
+//     console.log(err);
+// }
+
+// connection.end();
---
+++ SqlClass/schema.sql
@@ -0,0 +1,8 @@
+SHOW TABLES;
+
+-- CREATE TABLE user(
+--     id VARCHAR(50) PRIMARY KEY,
+--     name VARCHAR(50) NOT NULL,
+--     email VARCHAR(50) UNIQUE NOT NULL,
+--     password VARCHAR(50) NOT NULL
+-- );
